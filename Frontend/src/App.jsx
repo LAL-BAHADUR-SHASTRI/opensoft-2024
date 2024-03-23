3d5028dc8ab819f190032616f4bcfdae063137ab
Animesh <animeshraj958@gmail.com>
2024-03-23 07:25:52 +0530

    opensoft1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,23 @@
-import './App.css'
-import { Button } from './components/ui/button'
-import useWindowDimensions from './hooks/useWindowDimensions'
+import "./App.css";
+//import { Button } from './components/ui/button'
+import useWindowDimensions from "./hooks/useWindowDimensions";
+import Footer from "./components/ui/Footer";
+import SignIn from "./components/ui/SignIn";
+import NavBar from "./components/ui/NavBar";
 
 function App() {
-
-  const {height, width} = useWindowDimensions();
+  const { height, width } = useWindowDimensions();
 
   return (
-    <div style={{ height: height, backgroundColor: 'gray'}}>
+    <>
+      {/* <div style={{ height: height, backgroundColor: 'gray'}}>
       <Button style={{ margin: '50%'}}> hello </Button>
-    </div>
-  )
+    </div> */}
+      <NavBar />
+      <SignIn />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
