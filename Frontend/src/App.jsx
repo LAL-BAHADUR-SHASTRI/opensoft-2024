10a90d8f782a92e7bda8cdea2ef9bc98117fd577
gwydion67 <gwydionmaranio@gmail.com>
2024-03-24 23:22:23 +0530

    nav mar main page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
-import { Button } from './components/ui/button'
-import useWindowDimensions from './hooks/useWindowDimensions'
+import Nav from './components/Navbar'
+// import { Button } from './components/ui/button'
+// import useWindowDimensions from './hooks/useWindowDimensions'
 
 function App() {
 
-  const {height, width} = useWindowDimensions();
-
   return (
-    <div style={{ height: height, backgroundColor: 'gray'}}>
-      <Button style={{ margin: '50%'}}> hello </Button>
-    </div>
+    <>
+      <Nav />
+    </>
   )
 }
-
 export default App
---
+++ Frontend/src/components/Navbar/index.jsx
@@ -0,0 +1,116 @@
+import "./index.css"
+import logo from '../../assets/logo.svg'
+import { useState } from "react"
+import { LuSearch } from "react-icons/lu";
+import Stylesheet from "reactjs-stylesheet";
+
+//<--buttons-->
+
+const NavButtons = () => {
+  const [selected, setSelected] = useState('Home')
+
+  const buttons = ['Home','Movies','Series','Watchlist'];
+
+  const handlePress = (goto) => {
+    setSelected(goto);
+  }
+
+  return (
+    <div className="menuButtons" >
+      {buttons.map((item,index) => (
+        <text 
+          key={index}
+          style={item == selected ? styles.selectedStyle : {}}
+          onClick={() => handlePress(item)}
+          className="menuButton" >
+          {item}
+        </text>
+      ))}
+    </div>
+  )
+}
+
+//<--search-->
+
+const Search = ({isActive, setActive}) => {  // yar isko animate bhi karna hai .... baad me karunga
+  return isActive ?(
+    <>
+      <div style={styles.searchContainer}/>
+      <div style={{...styles.searchContainer, right: '25%' , position: "absolute", minWidth: '55%'}} >
+        <LuSearch 
+          size={28}
+          color="rgba(153, 153, 153, 1)"
+        />
+        <input onChange={() => console.log('hi')} placeholder="Search for Movies, Series and more..." className="dark:text-input searchbox "/>
+      </div>
+    </>
+  ) :(
+      <div onClick={() => setActive(!isActive)} style={styles.searchContainer}>
+        <LuSearch 
+          color="rgba(153, 153, 153, 1)"
+        />
+      </div>
+    )
+}
+
+//<--User-->
+const UserData = ({isLoggedin}) => {
+  if(isLoggedin){
+    return (
+    <></>
+    )
+  }
+  else {
+    return (
+      <div style={{display: 'flex', flexDirection: "row"}}>
+        <div className="signin" >Sign in</div>
+        <div className="signup" >Sign up</div>
+      </div>
+    )
+  }
+}
+
+//<--main-->
+
+const Nav = () => {
+
+  const [isActive, setActive] = useState(false)
+  const sLoggedin = false;
+  
+  return (
+    <nav className="navbar">
+      <img className="logo" src={logo} /> {/*logo*/}
+      <div style={{display: 'flex',flex: 1, flexDirection:'row', alignItems: 'center', justifyContent: 'space-evenly'}}>
+        <NavButtons />
+        <Search isActive={isActive} setActive={setActive}/>
+      </div>
+      <div />
+      <UserData />
+    </nav>
+  )
+}
+
+export default Nav;
+
+//<--styles--> // maybe remove these in future ?
+
+const styles = Stylesheet.create({
+  selectedStyle : {
+    borderBottomWidth: 3,
+    color: 'rgba(255, 255, 255, 1)',
+    borderBottomColor: 'rgba(240, 171, 0, 1)', 
+  },
+ searchContainer : {
+    display: 'flex',
+    zIndex: 10,
+    flexDirection: 'row',
+    padding: 10,
+    margin: 12,
+    backgroundColor: 'rgba(20,20,20,1)',
+    borderWidth: 1,
+    borderColor: 'rgba(50,50,50,1)',
+    borderRadius: 20,
+    alignItems: 'center'
+  },
+})
+
