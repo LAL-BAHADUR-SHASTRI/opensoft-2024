18e511b198e2018acc165811787a306392ead5eb
Animesh <animeshraj958@gmail.com>
2024-03-26 20:29:19 +0530

    changes

--- Frontend/src/components/ui/Purchase.jsx
+++ Frontend/src/components/ui/Purchase.jsx
@@ -9,18 +9,22 @@ function Purchase() {
                 <div className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-signin bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
                   <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
                 </div>
-                <div className="relative p-6 px-6 py-14 md:px-12">
-                  <h2 className="mb-6 block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased">
-                    How we design and code open-source projects?
-                  </h2>
-                  <h5 className="block mb-4 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-gray-400">
-                    Tania Andrew
-                  </h5>
+                <div className="flex">
                   <img
-                    alt="Tania Andrew"
-                    src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
-                    className="relative inline-block h-[74px] w-[74px] !rounded-full border-2 border-white object-cover object-center"
+                    alt="logo"
+                    src="logo.png"
+                    className="relative inline-block object-center w-80 h-40"
                   />
+                  <div className="relative p-6 px-6 py-14 md:px-12">
+                    <h2 className="mb-6 block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased">
+                      Upgrade your <span className="text-gold">viewing </span>
+                      experience
+                    </h2>
+                    <h5 className="block mb-4 font-sans text-xl antialiased leading-snug tracking-normal text-gray-400">
+                      Access to a wide selection of movies and shows, including
+                      new releases and exclusive content.{" "}
+                    </h5>
+                  </div>
                 </div>
               </div>
             </div>
