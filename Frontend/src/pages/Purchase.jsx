ae53357373758ce957e109ec5180e65e53a47bf5
sleep-lover <laleetsb2002@gmail.com>
2024-03-29 20:09:23 +0530

    feat the purchese page is done

--- Frontend/src/pages/Purchase.jsx
+++ Frontend/src/pages/Purchase.jsx
@@ -1,208 +1,221 @@
+import React from "react";
+
+function PlanCard({ name, description, price, duration, onClick }) {
+  return (
+    <div className="relative max-w-sm p-8 bg-gray-900 border border-gray-700 rounded-lg shadow shadow-blue-900">
+      {name === "Standard" ? (
+        <p className="absolute m-2 top-0 right-0 text-secondary-foreground bg-secondary  font-sm rounded-sm text-sm px-2 py-1 text-center">
+          Popular
+        </p>
+      ) : (
+        <></>
+      )}
+      <h5 className="mb-2 text-xl font-bold tracking-tight text-white text-left">
+        {name}
+      </h5>
+      <p className="mb-6 font-normal text-gray-400 text-left">{description}</p>
+      <h5 className="my-6 text-2xl tracking-tight text-white text-left">
+        ₹{price} <span className="text-gray-500 text-sm">/{duration}</span>
+      </h5>
+      <button
+        type="submit"
+        className="w-full text-secondary-foreground bg-secondary focus:ring-2  focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        onClick={onClick}>
+        Choose Plan
+      </button>
+    </div>
+  );
+}
+
 function Purchase() {
-    return (
-      <div className="bg-gray-600 p-8">
-        <div className="mx-auto rounded-xl overflow-hidden bg-gray-800 shadow-2xl shadow-blue-900 mt-20">
-          <div className="md:flex">
-            <p className="text-gray-500 w-full">
-              <div className="flex flex-col">
-                <div className="relative grid h-[25rem] flex-col items-end justify-center overflow-hidden bg-white bg-clip-border text-center text-gray-700">
-                  <div className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-signin bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
-                    <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
-                  </div>
-                  <div className="flex">
-                    <img
-                      alt="logo"
-                      src="logo.png"
-                      className="relative inline-block object-center w-80 h-40"
-                    />
-                    <div className="relative p-6 px-6 py-14 md:px-12">
-                      <h2 className="mb-6 block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased">
-                        Upgrade your <span className="text-gold">viewing </span>
-                        experience
-                      </h2>
-                      <h5 className="block mb-4 font-sans text-xl antialiased leading-snug tracking-normal text-gray-400">
-                        Access to a wide selection of movies and shows, including
-                        new releases and exclusive content.{" "}
-                      </h5>
-                    </div>
-                  </div>
-                </div>
-              </div>
+  const plans = [
+    {
+      name: "Starter",
+      price: "199",
+      duration: "month",
+      description:
+        "Access to a wide selection of movies and shows, including new releases and exclusive content.",
+      devices: "One device simultaneously",
+      offlineViewing: "NO",
+      hdr: "NO",
+      dolbyAtmos: "NO",
+      adFree: "NO",
+    },
+    {
+      name: "Standard",
+      price: "169",
+      duration: "month (rs1015 for 6 months)",
+      description:
+        "Access to a wide selection of movies and shows, including new releases and exclusive content and much more",
+      devices: "Two devices simultaneously",
+      offlineViewing: "NO",
+      hdr: "YES",
+      dolbyAtmos: "NO",
+      adFree: "YES",
+    },
+    {
+      name: "Premium",
+      price: "149",
+      duration: "month (rs1799 for 12 months)",
+      description:
+        "Access to a wide selection of movies and shows, including new releases and exclusive content and much more",
+      devices: "One device simultaneously",
+      offlineViewing: "YES",
+      hdr: "YES",
+      dolbyAtmos: "YES",
+      adFree: "YES",
+    },
+  ];
+
+  return (
+    <div className="bg-background px-8 py-2">
+      <div className=" w-full text-left text-sm ">
+        <div class="flex flex-row items-center justify-evenly gap-20 py-10">
+          <div className="w-30 h-30 bg-white">logo</div>
+          <div>
+            <h1 className="mt-4 text-4xl font-semi text-white">
+              Upgrade your <span className=" text-secondary">viewing</span>{" "}
+              experience
+            </h1>
+            <p className="mt-2 text-sm text-gray-300">
+              Access to a wider selection of movies and shows, including most
+              new releases and exclusive content
             </p>
           </div>
-          <p className="text-white p-10 text-4xl">
-            Choose the plan that&apos;s right for you
-          </p>
-          <div className="flex flex-wrap space-x-4 p-4 justify-evenly">
-            <div>
-              <div className="max-w-md p-6 bg-gray-900 border border-gray-700 rounded-lg shadow shadow-blue-900">
-                <a href="#">
-                  <h5 className="mb-2 text-xl font-bold tracking-tight text-white">
-                    Starter
-                  </h5>
-                </a>
-                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
-                  Access to a wide selection of movies and shows, including new
-                  releases and exclusive content.
-                </p>
-                <h5 className="my-11 text-3xl tracking-tight text-white">
-                  199 <span className="text-gray-500 text-sm">/month</span>
-                </h5>
-                <button
-                  type="submit"
-                  className="w-full text-black bg-gradient-to-r from-gold to-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                >
-                  Choose Plan
-                </button>
-              </div>
-            </div>
-            {/*  */}
-            <div>
-              <div className="max-w-md p-6 bg-gray-900 border border-gray-700 rounded-lg shadow shadow-blue-900">
-                <div className="flex justify-between">
-                  <a href="#">
-                    <h5 className="mb-2 text-xl font-bold tracking-tight text-white">
-                      Standard
-                    </h5>
-                  </a>
-                  <img src="premium.png" alt="premium" className="h-100 w-200" />
-                </div>
-                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
-                  Access to a wide selection of movies and shows, including new
-                  releases and exclusive content and much more
-                </p>
-                <h5 className="my-8 text-3xl tracking-tight text-white">
-                  169{" "}
-                  <span className="text-gray-500 text-sm">
-                    /month(rs1015 for 6 months)
-                  </span>
-                </h5>
-                <button
-                  type="submit"
-                  className="w-full text-black bg-gradient-to-r from-gold to-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                >
-                  Choose Plan
-                </button>
-              </div>
-            </div>
-            {/*  */}
-  
-            <div>
-              <div className="max-w-md p-6 bg-gray-900 border border-gray-700 rounded-lg shadow shadow-blue-900">
-                <a href="#">
-                  <h5 className="mb-2 text-xl font-bold tracking-tight text-white">
-                    Premium
-                  </h5>
-                </a>
-                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
-                  Access to a wide selection of movies and shows, including new
-                  releases and exclusive content and much more
-                </p>
-                <h5 className="my-8 text-3xl tracking-tight text-white">
-                  149{" "}
-                  <span className="text-gray-500 text-sm">
-                    /month(rs1799 for 12 months)
-                  </span>
-                </h5>
-                <button
-                  type="submit"
-                  className="w-full text-black bg-gradient-to-r from-gold to-yellow-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                >
-                  Choose Plan
-                </button>
-              </div>
-            </div>
-            {/*  */}
-          </div>
-          <p className="text-white p-10 text-4xl">
-            Compare our plans and find the right one for you
-          </p>
-          <div className="flex justify-center mx-40 my-10">
-            <table className="table-auto border-collapse rounded-lg">
-              <thead className="bg-indigo-900 text-white">
+        </div>
+      </div>
+
+      <div className="mx-auto rounded-xl overflow-hidden bg-background shadow-2xl shadow-blue-950">
+        {/* Plans */}
+        <p className="text-white p-10 text-3xl  font-semibold text-left ">
+          Choose the plan that's right for you
+        </p>
+        <div className="flex flex-wrap gap-8 space-x-4 p-4 justify-evenly">
+          {plans.map((plan, index) => (
+            <PlanCard key={index} {...plan} />
+          ))}
+        </div>
+
+        <p className="text-white p-10 text-3xl font-semibold text-left ">
+          Compare our plans and find the right one for you
+        </p>
+
+        <div className="flex justify-center mx-40 my-10 text-left text-sm  ">
+          <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-400">
+              <thead class="text-xs text-accent-foreground uppercase bg-accent">
                 <tr>
-                  <th className="border px-4 py-2">Features</th>
-                  <th className="border px-4 py-2">Starter</th>
-                  <th className="border px-4 py-2">
-                    <div className="flex">
-                      Standard{" "}
-                      <img
-                        src="premium.png"
-                        alt="premium"
-                        className="h-100 w-200 px-5"
-                      />
-                    </div>
+                  <th scope="col" class=" px-6 py-4 text-sm">
+                    Features
                   </th>
-                  <th className="border px-4 py-2">Premium</th>
+                  {plans.map((plan, index) => (
+                    <th key={index} scope="col" class=" px-6 py-3 ">
+                      <div className="flex flex-row items-center">
+                        <p className="">{plan.name}</p>
+                        {plan.name === "Standard" ? (
+                          <p className=" m-2 block text-secondary-foreground bg-secondary  font-sm rounded-sm text-sm px-2 py-1 text-center">
+                            Popular
+                          </p>
+                        ) : (
+                          <></>
+                        )}
+                      </div>
+                    </th>
+                  ))}
                 </tr>
               </thead>
-              <tbody className="text-gray-500">
-                <tr>
-                  <td className="border px-4 py-2">Price</td>
-                  <td className="border px-4 py-2">rs199/Month</td>
-                  <td className="border px-4 py-2">rs169/Month</td>
-                  <td className="border px-4 py-2">rs149/Month</td>
+              <tbody>
+                <tr class=" border-b ">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium  whitespace-nowrap">
+                    Price
+                  </th>
+                  {plans.map((plan, index) => (
+                    <td key={index} class="px-6 py-4">
+                      {plan.price} / {plan.duration}
+                    </td>
+                  ))}
                 </tr>
-                <tr>
-                  <td className="border px-4 py-2">Content</td>
-                  <td className="border px-4 py-2">
-                    {" "}
-                    Access to a wide selection of movies and shows, including new
-                    releases and exclusive content and much more
-                  </td>
-                  <td className="border px-4 py-2">
-                    {" "}
-                    Access to a wide selection of movies and shows, including new
-                    releases and exclusive content and much more
-                  </td>
-                  <td className="border px-4 py-2">
-                    {" "}
-                    Access to a wide selection of movies and shows, including new
-                    releases and exclusive content and much more
-                  </td>
+                <tr class=" border-b ">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium  whitespace-nowrap">
+                    Content
+                  </th>
+                  {plans.map((plan, index) => (
+                    <td key={index} class="px-6 py-4">
+                      {plan.description}
+                    </td>
+                  ))}
                 </tr>
-                <tr>
-                  <td className="border px-4 py-2"> Devices</td>
-                  <td className="border px-4 py-2">
-                    Watch on One device simultaneously
-                  </td>
-                  <td className="border px-4 py-2">
-                    Watch on Two device simultaneously
-                  </td>
-                  <td className="border px-4 py-2">
-                    Watch on One device simultaneously
-                  </td>
+                <tr class=" border-b ">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium  whitespace-nowrap">
+                    Devices
+                  </th>
+                  {plans.map((plan, index) => (
+                    <td key={index} class="px-6 py-4">
+                      {plan.devices}
+                    </td>
+                  ))}
                 </tr>
-                <tr>
-                  <td className="border px-4 py-2">Offline Viewing</td>
-                  <td className="border px-4 py-2">Basic 1</td>
-                  <td className="border px-4 py-2">Standard 1</td>
-                  <td className="border px-4 py-2">Premium 1</td>
+                <tr class=" border-b ">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium  whitespace-nowrap">
+                    Offline Viewing
+                  </th>
+                  {plans.map((plan, index) => (
+                    <td key={index} class="px-6 py-4">
+                      {plan.offlineViewing}
+                    </td>
+                  ))}
                 </tr>
-                <tr>
-                  <td className="border px-4 py-2"> HDR</td>
-                  <td className="border px-4 py-2">Basic 1</td>
-                  <td className="border px-4 py-2">Standard 1</td>
-                  <td className="border px-4 py-2">Premium 1</td>
+                <tr class=" border-b ">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium  whitespace-nowrap">
+                    HDR
+                  </th>
+                  {plans.map((plan, index) => (
+                    <td key={index} class="px-6 py-4">
+                      {plan.hdr}
+                    </td>
+                  ))}
                 </tr>
-                <tr>
-                  <td className="border px-4 py-2">Dolby Atmos</td>
-                  <td className="border px-4 py-2">Basic 1</td>
-                  <td className="border px-4 py-2">Standard 1</td>
-                  <td className="border px-4 py-2">Premium 1</td>
+                <tr class=" border-b ">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium  whitespace-nowrap">
+                    Dolby Atmos
+                  </th>
+                  {plans.map((plan, index) => (
+                    <td key={index} class="px-6 py-4">
+                      {plan.dolbyAtmos}
+                    </td>
+                  ))}
                 </tr>
-                <tr>
-                  <td className="border px-4 py-2">Ad-Free</td>
-                  <td className="border px-4 py-2">Basic 1</td>
-                  <td className="border px-4 py-2">Standard 1</td>
-                  <td className="border px-4 py-2">Premium 1</td>
+                <tr class=" border-b ">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium  whitespace-nowrap">
+                    Ad-Free
+                  </th>
+                  {plans.map((plan, index) => (
+                    <td key={index} class="px-6 py-4">
+                      {plan.adFree}
+                    </td>
+                  ))}
                 </tr>
               </tbody>
             </table>
           </div>
         </div>
       </div>
-    );
-  }
-  
-  export default Purchase;
+    </div>
+  );
+}
+
+export default Purchase;
