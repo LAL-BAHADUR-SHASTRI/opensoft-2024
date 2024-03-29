f7b786af8356449fbdf49405aedbebfd166fe0a6
gwydion67 <gwydionmaranio@gmail.com>
2024-03-29 22:25:06 +0530

    feat : add autocomplete feature in search box

---
+++ Frontend/src/components/Navbar/SearchResultCard.jsx
@@ -0,0 +1,16 @@
+import Stylesheet from "reactjs-stylesheet";
+
+
+const SearchResultCard = (data) => {
+  return (
+    <div>
+
+    </div>
+  )
+} 
+
+export default SearchResultCard;
+
+const styles = Stylesheet.create({
+
+})
