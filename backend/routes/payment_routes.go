c68782dc9ec083515fe5e6ce1bdb3b2ead8fbf1f
UtsavMandal2022 <utsav2016mandal@gmail.com>
2024-03-31 04:52:29 +0530

    payment logic fixed

--- backend/routes/payment_routes.go
+++ backend/routes/payment_routes.go
@@ -2,8 +2,10 @@ package routes
 
 import (
 	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
 	"opensoft_2024/database"
@@ -18,9 +20,36 @@ func PaymentServiceRouter(r *gin.Engine) {
 	{
 		payment.DELETE("/", DeleteAllPayments)
 		payment.POST("/create", CreatePayment)
-		payment.PUT("/update", UpdatePayment)
+		// payment.PUT("/update", UpdatePayment)
 		payment.GET("/", GetPayments)
 		payment.GET("/user/:user_id", GetPaymentByUserID)
+	}
+}
+
+func updateTier(userID string, planID string) {
+	// update tier in user collection
+	var newTier int
+	switch planID {
+	case "plan_NsGr1NWyUaaAhq":
+		newTier = 1
+	case "plan_NsGqpL2uyzi3Cw":
+		newTier = 2
+	case "plan_NsGqTUf5QdoVBn":
+		newTier = 3
+	}
+	objID, err1 := primitive.ObjectIDFromHex(userID)
+
+	if err1 != nil {
+		fmt.Println("Error converting user ID to ObjectID:", err1)
+	}
+
+	_, err := userCollection.UpdateOne(database.Ctx, bson.M{"_id": objID}, bson.M{
+		"$set": bson.M{
+			"tier": newTier,
+		},
+	})
+	if err != nil {
+		fmt.Println("Error updating user tier:", err)
 	}
 }
 
@@ -49,38 +78,45 @@ func CreatePayment(c *gin.Context) {
 
 	payment.ValidUpto = time.Now().AddDate(0, 1, 0)
 
+	// check if user already has a payment
+	filter := bson.M{"user_id": payment.UserID}
+	var existingPayment models.Payment
+	err := paymentCollection.FindOne(database.Ctx, filter).Decode(&existingPayment)
+	if err == nil {
+		// update existing payment
+		fmt.Println("Updating existing payment")
+		payment.ValidUpto = time.Now().AddDate(0, 1, 0)
+		filter := bson.M{"user_id": payment.UserID}
+		update := bson.M{
+			"$set": bson.M{
+				"user_id":    payment.UserID,
+				"plan_id":    payment.PlanID,
+				"valid_upto": payment.ValidUpto,
+			},
+		}
+	
+		updateTier(payment.UserID, payment.PlanID)
+	
+		result, err := paymentCollection.UpdateOne(database.Ctx, filter, update)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update payment"})
+			return
+		}
+	
+		c.JSON(http.StatusOK, result)
+		return
+	}
+
 	result, err := paymentCollection.InsertOne(database.Ctx, bson.M{
 		"user_id":    payment.UserID,
 		"plan_id":    payment.PlanID,
 		"valid_upto": payment.ValidUpto,
 	})
+
+	updateTier(payment.UserID, payment.PlanID)
+
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create payment"})
-		return
-	}
-
-	c.JSON(http.StatusOK, result)
-}
-
-func UpdatePayment(c *gin.Context) {
-	var payment models.Payment
-	if err := c.BindJSON(&payment); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	payment.ValidUpto = time.Now().AddDate(0, 1, 0)
-	filter := bson.M{"user_id": payment.UserID}
-	update := bson.M{
-		"$set": bson.M{
-			"user_id":    payment.UserID,
-			"plan_id":    payment.PlanID,
-			"valid_upto": payment.ValidUpto,
-		},
-	}
-
-	result, err := paymentCollection.UpdateOne(database.Ctx, filter, update)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update payment"})
 		return
 	}
 
